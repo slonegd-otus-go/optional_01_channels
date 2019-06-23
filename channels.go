345589dfc1753ccd63c7d601bab03ccc42310831
slonegd <slonkdv@mail.ru>
2019-06-23 10:08:34 +0300

    realized on goroutine

---
+++ channels.go
@@ -0,0 +1,18 @@
+package channels
+
+func Merge(args ... <-chan interface{}) <-chan interface{} {
+	out := make(chan interface{})
+	for _, channel := range args {
+		go func(channel <-chan interface{}) {
+			for {
+				value, ok <- channel
+				if !ok {
+					return
+				}
+				out <- value
+			}
+		}(channel)
+	}
+	return out
+}
+
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/slonegd-otus-go/optional_01_channels
+
+require github.com/slonegd-otus-go/06_list v0.0.0-20190615141743-f33abe6b5a55 // indirect
